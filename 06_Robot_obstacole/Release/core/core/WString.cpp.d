fa88e512585b36c944cd113eff260e4027cced2c
tgtn <38745911+tgtn@users.noreply.github.com>
2018-04-26 09:43:48 +0300

    added 06

---
+++ 06_Robot_obstacole/Release/core/core/WString.cpp.d
@@ -0,0 +1,5 @@
+core/core/WString.cpp.o: \
+ /Users/teddy/work/poken/programs/EclipseOxygen/Eclipse.app/Contents/Eclipse/arduinoPlugin/packages/arduino/hardware/avr/1.6.21/cores/arduino/WString.cpp \
+ /Users/teddy/work/poken/programs/EclipseOxygen/Eclipse.app/Contents/Eclipse/arduinoPlugin/packages/arduino/hardware/avr/1.6.21/cores/arduino/WString.h
+
+/Users/teddy/work/poken/programs/EclipseOxygen/Eclipse.app/Contents/Eclipse/arduinoPlugin/packages/arduino/hardware/avr/1.6.21/cores/arduino/WString.h:
