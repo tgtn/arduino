6525ac9f5915d90c77e6bb4230bb53c7e14cf7ee
tgtn <tgutan@gmail.com>
2018-04-26 11:00:28 +0300

    test commit

--- 01_LED_ON_OFF_digital_write/01_LED_ON_OFF_digital_write.ino
+++ 01_LED_ON_OFF_digital_write/01_LED_ON_OFF_digital_write.ino
@@ -2,12 +2,12 @@
 
 //The setup function is called once at startup of the sketch
 void setup() {
-  // put your setup code here, to run once:
+  // put your setup code here, to run once
   pinMode(LED_BUILTIN, OUTPUT);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  // put your main code here, to run repeatedly
   digitalWrite(LED_BUILTIN, HIGH);
   delay(1000);
   digitalWrite(LED_BUILTIN, LOW);
