d4aaeb2cabdec6b9903211b043ac7bb6e772865c
tgtn <38745911+tgtn@users.noreply.github.com>
2018-05-08 12:07:04 +0300

    Delete wiring_pulse.S.d

--- 06_Robot_obstacole/Release/core/core/wiring_pulse.S.d
+++
@@ -1,2 +0,0 @@
-core/core/wiring_pulse.S.o: \
- /Users/teddy/work/poken/programs/EclipseOxygen/Eclipse.app/Contents/Eclipse/arduinoPlugin/packages/arduino/hardware/avr/1.6.21/cores/arduino/wiring_pulse.S
